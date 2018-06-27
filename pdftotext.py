eb9d26f985942d396aeab9e74d9dcdd048c62784
Vinayak <demonoidvk@gmail.com>
2018-06-27 23:42:43 +0530

    Update pdftotext.py

--- pdftotext.py
+++ pdftotext.py
@@ -1,20 +1,12 @@
 
-# coding: utf-8
+import subprocess, os       #Library to execute terminal or commandline commands
+import glob                 #Library to import multiple files, sorted on bases of size
 
-# In[1]:
-
-
-import subprocess, os
-import glob
-
-
-# In[ ]:
-
-
-pdf_files =glob.glob("./data1/*.pdf")
+pdf_files =glob.glob("<folder_path>/*.pdf")   #importing a list of file
 for p in pdf_files:
     cmd= "pdftotext " + p 
     p=subprocess.Popen(cmd,shell=True, stdout=subprocess.PIPE)
     p.wait()
+    print(p)
     
 
